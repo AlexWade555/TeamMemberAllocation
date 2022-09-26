203de388cca91f7e91add15f4df080ca03d52333
Alex Wade <alexjohnwade@gmail.com>
2022-09-26 16:51:48 +1000

    added bootsrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
+import Header from './components/Header';
+import Content from './components/Content'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
