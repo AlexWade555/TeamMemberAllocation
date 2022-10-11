21a4a13e520d4fa8e34f3151e325e75d806dce1e
Alex Wade <alexjohnwade@gmail.com>
2022-10-11 19:16:26 +1100

    final touches

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <nav className = "navbar navbar-expand-lg navbar-light bg-light">
-      <ul className = "navbar-nav me-auto mb-2 mb-lg-0">
-        <li className = "nav-item">
+      <ul className = "navbar-nav me-auto mb-2 mb-lg-0" key ="nav">
+        <li className = "nav-item" key = "home">
           <Link className = "nav-link" to = "/">Home</Link>
         </li>
-        <li className = "nav-item">
+        <li className = "nav-item" key = "teams">
           <Link className = "nav-link" to = "/GroupedTeamMembers">Teams</Link>
         </li>
       </ul>
