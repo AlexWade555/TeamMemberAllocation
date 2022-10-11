ef238e6f8d95d28adc56d0ebcdadfbcb3a87b250
Alex Wade <alexjohnwade@gmail.com>
2022-10-11 18:10:01 +1100

    fixed bug

--- src/components/GroupedTeamMembers.jsx
+++ src/components/GroupedTeamMembers.jsx
@@ -4,30 +4,32 @@ const GroupedTeamMembers = ({employees, selectedTeam, setTeam}) => {
 
   const [groupedEmployees, setGroupedData] = useState(groupTeamMembers());
 
-  function groupTeamMembers()
-  {
+  function groupTeamMembers() {
     var teams = [];
 
     var teamAMembers = employees.filter((employee) => employee.teamName === 'TeamA');
-    var teamA = {team:'TeamA', members:teamAMembers, collapsed: selectedTeam === 'TeamA'?false:true}
+    var teamA = {team:'TeamA', members:teamAMembers, collapsed: selectedTeam === 'TeamA' ? false:true}
     teams.push(teamA);
 
     var teamBMembers = employees.filter((employee) => employee.teamName === 'TeamB');
-    var teamB = {team:'TeamB', members:teamBMembers, collapsed: selectedTeam === 'TeamB'?false:true}
+    var teamB = {team:'TeamB', members:teamBMembers, collapsed: selectedTeam === 'TeamB' ? false:true}
     teams.push(teamB);
 
     var teamCMembers = employees.filter((employee) => employee.teamName === 'TeamC');
-    var teamC = {team:'TeamC', members:teamCMembers, collapsed: selectedTeam === 'TeamC'?false:true}
+    var teamC = {team:'TeamC', members:teamCMembers, collapsed: selectedTeam === 'TeamC' ? false:true}
     teams.push(teamC);
 
     var teamDMembers = employees.filter((employee) => employee.teamName === 'TeamD');
-    var teamD = {team:'TeamD', members:teamDMembers, collapsed: selectedTeam === 'TeamD'?false:true}
+    var teamD = {team:'TeamD', members:teamDMembers, collapsed: selectedTeam === 'TeamD' ? false:true}
     teams.push(teamD);
+
+    return teams;
+
   }
 
   function handleTeamClick(event) {
     var transformedGroupData = groupedEmployees.map((groupedData) => groupedData.team === event.currentTarget.id
-                                                              ?{...groupedData,collapsed:!groupedData.collasped}
+                                                              ?{...groupedData,collapsed: !groupedData.collapsed}
                                                               :groupedData);
     setGroupedData(transformedGroupData);
     setTeam(event.currentTarget.id);
@@ -43,7 +45,7 @@ const GroupedTeamMembers = ({employees, selectedTeam, setTeam}) => {
                 Team Name: {item.team}
               </h4>
               <div id = {"collapse_" + item.team}
-                        className={item.collapsed === true? "collapsed":""}>
+                        className={item.collapsed === true? "collapse":""}>
                   <hr/>
                   {
                     item.members.map(member => {
@@ -58,8 +60,9 @@ const GroupedTeamMembers = ({employees, selectedTeam, setTeam}) => {
                     })
                   }
               </div>
+              <hr />
             </div>
-          )
+          );
         })
       }
     </main>
