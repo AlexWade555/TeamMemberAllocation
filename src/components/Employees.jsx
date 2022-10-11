60d969503151e0ac66b468aa91ade7aeb4f0e543
Alex Wade <alexjohnwade@gmail.com>
2022-10-11 13:30:35 +1100

    got rid of console errors

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -15,12 +15,12 @@ const Employees = ({employees,selectedTeam,handleEmployeeCardClick,handleTeamSel
           </select>
         </div>
       </div>
-      <div class="row justify-content-center mt-3 mb-3">
-        <div class ="col-8">
-          <div class="card-collection">
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className ="col-8">
+          <div className="card-collection">
             {
             employees.map((employee) => (
-              <div id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout':'card m-2')} style={{ cursor: "pointer"}} onClick ={handleEmployeeCardClick}>
+              <div key={employee.id} id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout':'card m-2')} style={{ cursor: "pointer"}} onClick ={handleEmployeeCardClick}>
                 {(employee.gender === 'male')?<img src={maleProfile} className="card-img-top" alt="male profile"/>
                                               :<img src={femaleProfile} className="card-img-top" alt="female profile"/>}
                 <div className='card-body'>
