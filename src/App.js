58bd0f29d213095764687186bed5b9fad31aff96
Alex Wade <alexjohnwade@gmail.com>
2022-10-11 16:06:04 +1100

    added grouped mmebers code

--- src/App.js
+++ src/App.js
@@ -136,7 +136,8 @@ function App() {
                  />}>
 
         </Route>
-        <Route path="/GroupedTeamMembers" element = {<GroupedTeamMembers/>}>
+        <Route path="/GroupedTeamMembers" element = {<GroupedTeamMembers employees = {employees}
+                                                      selectedTeam = {selectedTeam} setTeam = {setTeam} />}>
         </Route>
         <Route path="*" element = {<NotFound />}>
         </Route>
